ab190a0611f062c1eac8a606bbaed8929edc060b
christylaguardia <christinelaguardia@gmail.com>
2017-02-25 17:39:33 -0800

    Moved styling to css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // do this on page load
-var headers = ["Store Name", "Min Cust/Hr", "Max Cust/Hr", "Avg Cookies Sold", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "Total"];
+var headers = ["Store Name", "Min Cust", "Max Cust", "Avg Cookies Sold", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "Total"];
 var stores = [];
 stores.push(new store("Pioneer Square", 17, 88, 5.2));
 stores.push(new store("Portland Airport", 6, 24, 1.2));
@@ -38,11 +38,47 @@ function store(name, minCust, maxCust, avgCookie) {
     }
       // put the total in the array
       simulationArray.push(["","",simulationTotal]);
-      console.log("simulated sales array for " + this.name + ":");
-      console.log(simulationArray);
+      // console.log("simulated sales array for " + this.name + ":");
+      // console.log(simulationArray);
       return simulationArray;
   };
   console.log("store object created: " + this.name);
+}
+
+function buildTableRow(store) {
+  var newRow = document.createElement("tr");
+  // assign an id to the row
+  newRow.setAttribute("id", store);
+  // add the row to the table
+  document.getElementById("simulatedSalesData").appendChild(newRow);
+  // lookup type of
+  var rowCell1 = document.createElement("td");
+  var rowNode1 = document.createTextNode(store.name);
+  rowCell1.appendChild(rowNode1);
+  document.getElementById("simulatedSalesData").appendChild(rowCell1);
+
+  var rowCell2 = document.createElement("td");
+  var rowNode2 = document.createTextNode(store.minCust);
+  rowCell2.appendChild(rowNode2);
+  document.getElementById("simulatedSalesData").appendChild(rowCell2);
+
+  var rowCell3 = document.createElement("td");
+  var rowNode3 = document.createTextNode(store.maxCust);
+  rowCell3.appendChild(rowNode3);
+  document.getElementById("simulatedSalesData").appendChild(rowCell3);
+
+  var rowCell4 = document.createElement("td");
+  var rowNode4 = document.createTextNode(store.avgCookie);
+  rowCell4.appendChild(rowNode4);
+  document.getElementById("simulatedSalesData").appendChild(rowCell4);
+
+  var simulationResults = store.simulatedSales();
+  for (var i = 0; i < simulationResults.length; i++) {
+    var rowCell = document.createElement("td");
+    var rowNode = document.createTextNode(simulationResults[i][2]);
+    rowCell.appendChild(rowNode);
+    document.getElementById("simulatedSalesData").appendChild(rowCell);
+  }
 }
 
 function buildTable() {
@@ -54,30 +90,9 @@ function buildTable() {
     headerCell.appendChild(headerNode);
     document.getElementById("simulatedSalesData").appendChild(headerCell);
   }
-
-  // loop through each store in the stores array
+  // loop through each store in the stores array, and and add row to table
   for (var i = 0; i < stores.length; i++) {
-    // make a new row
-    var newRow = document.createElement("tr");
-    // assign an id to the row
-    newRow.setAttribute("id", "store" + i);
-    // add the row to the table
-    document.getElementById("simulatedSalesData").appendChild(newRow);
-    // loop through properties in object
-    for (var property in stores[i]) {
-      if (property !== "simulatedSales") {
-        // make a new cell
-        var newCell = document.createElement("td");
-        var newNode = document.createTextNode(stores[i][property]);
-        // put the property value in the cell
-        newCell.appendChild(newNode);
-        // add the cell to the row
-        document.getElementById("store" + i).appendChild(newCell);
-      } else {
-
-      }
-    }
-    console.log("row built for " + stores[i].name + " with the id: store" + i);
+    buildTableRow(stores[i]);
   }
   console.log("table built!")
 }
