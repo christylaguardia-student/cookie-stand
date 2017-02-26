69d43d3fb967e812a5909fe2132a5ec53516b6c2
christylaguardia <christinelaguardia@gmail.com>
2017-02-26 13:49:07 -0800

    Updated buildTableRow function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // do this on page load
-var headers = ["Store Name", "Min Cust", "Max Cust", "Avg Cookies Sold", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "Total"];
+var headers = ["Store Name", "Min Cust/Hr", "Max Cust/Hr", "Avg Cookies Sold/Hr", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "Total"];
 var stores = [];
 stores.push(new store("Pioneer Square", 17, 88, 5.2));
 stores.push(new store("Portland Airport", 6, 24, 1.2));
@@ -46,53 +46,50 @@ function store(name, minCust, maxCust, avgCookie) {
 }
 
 function buildTableRow(store) {
+  // create a new row
   var newRow = document.createElement("tr");
-  // assign an id to the row
   newRow.setAttribute("id", store);
-  // add the row to the table
   document.getElementById("simulatedSalesData").appendChild(newRow);
-  // lookup type of
-  var rowCell1 = document.createElement("td");
-  var rowNode1 = document.createTextNode(store.name);
-  rowCell1.appendChild(rowNode1);
-  document.getElementById("simulatedSalesData").appendChild(rowCell1);
 
-  var rowCell2 = document.createElement("td");
-  var rowNode2 = document.createTextNode(store.minCust);
-  rowCell2.appendChild(rowNode2);
-  document.getElementById("simulatedSalesData").appendChild(rowCell2);
+  // loop through each property in the object
+  for (var property in store) {
+    // if (typeof property !== "function") { // QUESTION: why does this always return a string?
+    // check which property
+    if (property != "simulatedSales") {
+      // do this for the string and number properties
+      var rowCell = document.createElement("td");
+      var rowNode = document.createTextNode(store[property]);
+      rowCell.appendChild(rowNode);
+      document.getElementById("simulatedSalesData").appendChild(rowCell);
+    } else {
+      // get the calculated cookie quantities
+      var simulationResults = store.simulatedSales();
 
-  var rowCell3 = document.createElement("td");
-  var rowNode3 = document.createTextNode(store.maxCust);
-  rowCell3.appendChild(rowNode3);
-  document.getElementById("simulatedSalesData").appendChild(rowCell3);
-
-  var rowCell4 = document.createElement("td");
-  var rowNode4 = document.createTextNode(store.avgCookie);
-  rowCell4.appendChild(rowNode4);
-  document.getElementById("simulatedSalesData").appendChild(rowCell4);
-
-  var simulationResults = store.simulatedSales();
-  for (var i = 0; i < simulationResults.length; i++) {
-    var rowCell = document.createElement("td");
-    var rowNode = document.createTextNode(simulationResults[i][2]);
-    rowCell.appendChild(rowNode);
-    document.getElementById("simulatedSalesData").appendChild(rowCell);
+      // loop through each cookie quantity in the array
+      for (var i = 0; i < simulationResults.length; i++) {
+        var rowCell = document.createElement("td");
+        var rowNode = document.createTextNode(simulationResults[i][2]); // the qty is the 3rd item in the sub-array
+        rowCell.appendChild(rowNode);
+        document.getElementById("simulatedSalesData").appendChild(rowCell);
+      }
+    }
   }
 }
 
 function buildTable() {
-  // add headers
+  // add header row
   var headerRow = document.createElement("tr");
+  // loop through headers array and add cell to header row
   for (var i = 0; i < headers.length; i++) {
     var headerCell = document.createElement("th");
     var headerNode = document.createTextNode(headers[i]);
     headerCell.appendChild(headerNode);
     document.getElementById("simulatedSalesData").appendChild(headerCell);
   }
+  console.log ("headers added");
   // loop through each store in the stores array, and and add row to table
   for (var i = 0; i < stores.length; i++) {
     buildTableRow(stores[i]);
   }
-  console.log("table built!")
+  console.log("table built!");
 }
